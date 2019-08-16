658c7dc255ff8bd8a68220768cc9b3ac4adef181
akels <akels14@gmail.com>
2019-08-17 00:12:03 +0200

    fix Travis

--- examples/sphere.jl
+++ examples/sphere.jl
@@ -1,21 +1,20 @@
-# A method for generating sphere mesh from subdivissions of icosahedron
 using GeometryTypes
 using LinearAlgebra
 
 """
     subdivide(msh::HomogenousMesh,f::Function)
 
-Returns a subdived triangular mesh from passed mesh `msh` and interpolator function `f`. The interpolator `f` is expected to accept two vertex indicies of the edge and to return a tuple of coordinates of the middle point (as one wishes to define it). 
+Returns a subdived triangular mesh from passed mesh `msh` and interpolator function `f`. The interpolator `f` is expected to accept two vertex indicies of the edge and to return a tuple of coordinates of the middle point (as one wishes to define it).
 """
 function subdivide(msh::HomogenousMesh,f::Function)
-    edges = filter(x->x[1]<x[2],decompose(Face{2,Int},msh)) 
+    edges = filter(x->x[1]<x[2],decompose(Face{2,Int},msh))
     epoint(v1,v2) = findfirst(x->x==(v2>v1 ? Face(v1,v2) : Face(v2,v1)), edges) + length(msh.vertices)
-            
+
     newfaces = Face{3,Int}[]
 
     newvertices = copy(msh.vertices)
     resize!(newvertices,length(msh.vertices) + length(edges))
-    
+
     for (v1,v2,v3) in msh.faces
 
         ev3 = epoint(v1,v2)
@@ -39,7 +38,7 @@ end
 """
     unitsphere(n)
 
-Returns a sphere mesh made out of icosahedron subdivided `n` times. 
+Returns a sphere mesh made out of icosahedron subdivided `n` times.
 """
 function unitsphere(subdivissions::Int64)
     t = ( 1 + sqrt( 5 ) ) / 2;
@@ -51,9 +50,9 @@ function unitsphere(subdivissions::Int64)
     ]
 
     faces = Face{3,Int64}[
-        [1, 12, 6], [1, 6, 2], [1, 2, 8], [1, 8, 11], [1, 11, 12], [2, 6, 10], [6, 12, 5], 
-        [12, 11, 3], [11, 8, 7], [8, 2, 9], [4, 10, 5], [4, 5, 3], [4, 3, 7], [4, 7, 9],  
-        [4, 9, 10], [5, 10, 6], [3, 5, 12], [7, 3, 11], [9, 7, 8], [10, 9, 2] 
+        [1, 12, 6], [1, 6, 2], [1, 2, 8], [1, 8, 11], [1, 11, 12], [2, 6, 10], [6, 12, 5],
+        [12, 11, 3], [11, 8, 7], [8, 2, 9], [4, 10, 5], [4, 5, 3], [4, 3, 7], [4, 7, 9],
+        [4, 9, 10], [5, 10, 6], [3, 5, 12], [7, 3, 11], [9, 7, 8], [10, 9, 2]
     ]
 
     msh = HomogenousMesh(vertices ./ sqrt(1+t^2),faces)
@@ -61,7 +60,7 @@ function unitsphere(subdivissions::Int64)
     function sf(msh,v1,v2)
         p1 = msh.vertices[v1]
         p2 = msh.vertices[v2]
-        return (p1 + p2)/norm(p1+p2) 
+        return (p1 + p2)/norm(p1+p2)
     end
 
 
@@ -71,3 +70,4 @@ function unitsphere(subdivissions::Int64)
 
     return msh
 end
+
